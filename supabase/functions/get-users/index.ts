6f46195bc3a19f6b3db31243f8292f22e7fd8a65
Elijah Mondero <elijahmondero@gmail.com>
2026-01-11 00:32:09 +1300

    fix: use different secret

--- supabase/functions/get-users/index.ts
+++ supabase/functions/get-users/index.ts
@@ -33,7 +33,7 @@ serve(async (req) => {
 
     const supabaseAdmin = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
-      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+      Deno.env.get('SERVICE_ROLE_KEY') ?? ''
     )
 
     const { data: users, error } = await supabaseAdmin.auth.admin.listUsers()
