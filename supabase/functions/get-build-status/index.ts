d4da47195a87c8c5da17538138754ae3210959bb
Elijah Mondero <elijahmondero@gmail.com>
2026-01-17 01:41:44 +1300

    fix: allow post

--- supabase/functions/get-build-status/index.ts
+++ supabase/functions/get-build-status/index.ts
@@ -6,7 +6,7 @@ const GITHUB_REPO = 'SantoNinoNZ.github.io'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
